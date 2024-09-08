32637fb413fa3989ac0cd8bf9682f1addbe67e21
Muhammad Fadhlan Karimuddin <138226358+poemich@users.noreply.github.com>
2024-09-08 18:04:40 +0700

    Add initial files and configurations

---
+++ pasar_barang_pilihan/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for pasar_barang_pilihan project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pasar_barang_pilihan.settings')
+
+application = get_wsgi_application()
