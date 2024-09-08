a7d1215c018532a5cafd13ee23e31ea45d2b371e
Muhammad Fadhlan Karimuddin <138226358+poemich@users.noreply.github.com>
2024-09-08 18:08:17 +0700

    Update ALLOWED_HOSTS in settings.py

--- pasar_barang_pilihan/settings.py
+++ pasar_barang_pilihan/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&uy=!ywj1q^6g2exf#vem40=)2ipt6f%h-6x#-0@+4f*q=e_d_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://muhammad-fadhlan31-pasarbarangpilihan.pbp.cs.ui.ac.id/"]
 
 
 # Application definition
