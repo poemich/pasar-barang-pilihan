b0deab2660cd885324d029d629493ec4b2577816
Muhammad Fadhlan Karimuddin <138226358+poemich@users.noreply.github.com>
2024-09-09 22:53:07 +0700

    make README file

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-# Create your models here.
 class Product(models.Model):
-    name = models.TextField()
-    price = models.FloatField()
-    description = models.TextField()
+    class Product(models.Model):
+        name = models.CharField(max_length=100)
+        price = models.IntegerField()
+        description = models.TextField()
